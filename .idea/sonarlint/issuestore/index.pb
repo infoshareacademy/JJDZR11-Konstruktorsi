14e464454e7e35e61251565e44316ec5b62696c7
“przemek <pwenderholm@gmail.com>
2023-03-13 14:03:42 +0100

    dodanie metidy uwuwaia i dodawnia ksiazek

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,10 @@
+
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
+`
+0src/main/java/pl/isa/biblioteka/FolderBooks.java,1/2/120250e7ee76c4082922fcd04c2bc3b204649f67
+]
+-src/main/java/pl/isa/biblioteka/AddUsers.java,d/7/d7fc7a4802f2ad74be19e495e51e89effd5e635b
+Y
+)src/main/java/pl/isa/biblioteka/Menu.java,4/0/407b9f0b111210c98d253b1ef90acab50702b46b
--- src/main/java/pl/isa/biblioteka/BooksEdit.java
+++ src/main/java/pl/isa/biblioteka/BooksEdit.java
@@ -1,5 +1,35 @@
 package pl.isa.biblioteka;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class BooksEdit {
     //Przemysław Wenderholm
+//    TASK TO DO:
+//    Jako bibliotekarz mam możliwość edycji katalogu książek, poprzez dodanie nowej książki lub usunięcie istniejącej.
+
+    private List<Book> books = new ArrayList<>();
+
+    public void addBook(Book book){
+            books.add(book);
+    }
+
+    public void deleteBookByTitle(List<Book> books, String title){
+        if (!books.removeIf(book -> book.getTitle().equalsIgnoreCase(title))) {
+            System.out.println("nie ma takiej książki ");
+        }
+    }
+
+    public void showBooks(List<Book> books){
+        for (Book book : books) {
+            System.out.println(book);
+        }
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
 }
