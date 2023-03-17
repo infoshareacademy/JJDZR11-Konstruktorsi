f2f833d878b1e73b8d29a15dade2c5da14f1d433
MMalinowski19 <mikolajmalinowski19@gmail.com>
2023-03-18 00:04:28 +0100

    Dodawanie nowego uzytkownika, przeglad zapisanych uzytkownikow

--- src/main/java/pl/isa/biblioteka/AddUsers.java
+++ src/main/java/pl/isa/biblioteka/AddUsers.java
@@ -1,5 +1,30 @@
 package pl.isa.biblioteka;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AddUsers {
     //Mikołaj Malinowski
+    //Dodanie nowego użytkownika
+    private String firstName;
+    private String lastName;
+    private String firstNameAndLastName;
+    ArrayList<String> users = new ArrayList<>();
+    public void addUser (){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Podaj imię nowego użytkownika: ");
+        firstName = input.nextLine();
+        System.out.println("Podaj nazwisko nowego użytkownika: ");
+        lastName = input.nextLine();
+        firstNameAndLastName = firstName + " " + lastName;
+        users.add(firstNameAndLastName);
+        System.out.println("Dodano nowego użytkownika: "+firstNameAndLastName);
+    }
+    public void ListOfUsers(){
+        System.out.println("Użytkownicy: ");
+        for (int i=0; i<users.size(); i++){
+            System.out.println(i+1+" "+users.get(i));
+        }
+
+    }
 }
