6605f783c0aeea9d1de6cbacf910d0bda7161678
MMalinowski19 <mikolajmalinowski19@gmail.com>
2023-03-19 18:02:45 +0100

    poprawki do PR

--- src/main/java/pl/isa/biblioteka/AddUsers.java
+++ src/main/java/pl/isa/biblioteka/AddUsers.java
@@ -6,24 +6,27 @@ import java.util.Scanner;
 public class AddUsers {
     //Mikołaj Malinowski
     //Dodanie nowego użytkownika
-    private String firstName;
-    private String lastName;
-    private String firstNameAndLastName;
     ArrayList<String> users = new ArrayList<>();
-    public void addUser (){
+
+    public void addUser() {
+        String firstName;
+        String lastName;
+        String name;
+
         Scanner input = new Scanner(System.in);
         System.out.println("Podaj imię nowego użytkownika: ");
         firstName = input.nextLine();
         System.out.println("Podaj nazwisko nowego użytkownika: ");
         lastName = input.nextLine();
-        firstNameAndLastName = firstName + " " + lastName;
-        users.add(firstNameAndLastName);
-        System.out.println("Dodano nowego użytkownika: "+firstNameAndLastName);
+        name = firstName + " " + lastName;
+        users.add(name);
+        System.out.println("Dodano nowego użytkownika: " + name);
     }
-    public void ListOfUsers(){
+
+    public void listUsers() {
         System.out.println("Użytkownicy: ");
-        for (int i=0; i<users.size(); i++){
-            System.out.println(i+1+" "+users.get(i));
+        for (int i = 0; i < users.size(); i++) {
+            System.out.println(i + 1 + " " + users.get(i));
         }
 
     }
