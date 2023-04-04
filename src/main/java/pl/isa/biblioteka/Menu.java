d6d23ac9e44185a96cb107375c70ba85719bb3b0
KozlowskiKamil <kamil2k8@gmail.com>
2023-04-04 12:11:32 +0200

    Poprawa wymaganych zmian do PR

--- src/main/java/pl/isa/biblioteka/Menu.java
+++ src/main/java/pl/isa/biblioteka/Menu.java
@@ -1,50 +1,52 @@
 package pl.isa.biblioteka;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Menu {
-    public void selectUser() {
-        System.out.println("\nWitamy w naszej bibliotece");
-        System.out.println("Kim jesteś ?\nWybierz odpowiednią opcję w menu");
-        System.out.println("1. Bibliotekarz");
-        System.out.println("2. Użytkownik");
+    protected void selectUser() {
+        System.out.println("Witamy w naszej bibliotece\nKim jesteś ?");
         Scanner scanner = new Scanner(System.in);
         boolean isContinue = true;
         while (isContinue) {
+            System.out.println("Wybierz odpowiednią opcję w menu");
+            System.out.println("1 - Bibliotekarz");
+            System.out.println("2 - Użytkownik");
+            System.out.println("3 - Zakończ program");
             try {
                 int userChoose = scanner.nextInt();
                 if (userChoose == 1) {
                     librarianMenu();
                 } else if (userChoose == 2) {
                     userMenu();
+                } else if (userChoose == 3) {
+                    isContinue = false;
                 } else {
-                    System.out.println("Wprowadź poprawny numer");
-                    selectUser();
+                    System.out.println("Został wprowadzony niewłaściwy numer\n");
                 }
-            } catch (Exception e) {
-                System.out.println("Wprowadziłeś niepoprawny znak");
-                selectUser();
+            } catch (InputMismatchException e) {
+                System.out.println("Został wprowadzony niewłaściwy znak\n");
+                scanner.next();
             }
-            isContinue = false;
         }
     }
 
-    public void librarianMenu() {
+    protected void librarianMenu() {
         Users users = new Users();
         BooksEdit booksEdit = new BooksEdit();
         Scanner scanner = new Scanner(System.in);
-        try {
-            boolean isContinue = true;
-            while (isContinue) {
-                System.out.println("\nWybierz numer menu");
-                System.out.println("1. Widok książek");
-                System.out.println("2. Dodaj użytkownika");
-                System.out.println("3. Usuń użytkownika");
-                System.out.println("4. Lista użytkowników");
-                System.out.println("5. Dodaj książkę");
-                System.out.println("6. Usuń książkę");
-                System.out.println("7. Zmień użytkownika");
-                System.out.println("8. Zakończ program");
+        boolean isContinue = true;
+        while (isContinue) {
+            System.out.println("\nWybierz numer menu");
+            System.out.println("1 - Widok książek");
+            System.out.println("2 - Dodaj użytkownika");
+            System.out.println("3 - Usuń użytkownika");
+            System.out.println("4 - Lista użytkowników");
+            System.out.println("5 - Dodaj książkę");
+            System.out.println("6 - Usuń książkę");
+            System.out.println("7 - Zmień użytkownika");
+            System.out.println("8 - Zakończ program");
+            try {
                 int userChoose = scanner.nextInt();
                 if (userChoose > 0 && userChoose < 9) {
                     switch (userChoose) {
@@ -60,31 +62,31 @@ public class Menu {
                             FolderBooks.saveBooks();
                             System.out.println("Baza użytkowników i książek zapisana poprawnie");
                             isContinue = false;
+                            System.exit(0);
                         }
                     }
                 } else {
-                    System.out.println("Wybierz poprawny numer menu");
-                    continue;
+                    System.out.println("Został wprowadzony niewłaściwy numer.");
                 }
+            } catch (InputMismatchException e) {
+                System.out.println("Został wprowadzony niewłaściwy znak.");
+                scanner.next();
             }
-        } catch (Exception e) {
-            System.out.println("Wprowadziłeś niepoprawny znak");
-            librarianMenu();
         }
     }
 
-    public void userMenu() {
+    protected void userMenu() {
         BooksEdit booksEdit = new BooksEdit();
         BorrowBooks borrowBooks = new BorrowBooks();
         Scanner scanner = new Scanner(System.in);
-        try {
-            boolean isContinue = true;
-            while (isContinue) {
-                System.out.println("\nWybierz numer menu");
-                System.out.println("1. Widok książek");
-                System.out.println("2. Operacje na książkach");
-                System.out.println("3. Zmień użytkownika");
-                System.out.println("4. Zakończ program");
+        boolean isContinue = true;
+        while (isContinue) {
+            System.out.println("\nWybierz numer menu");
+            System.out.println("1 - Widok książek");
+            System.out.println("2 - Operacje na książkach");
+            System.out.println("3 - Zmień użytkownika");
+            System.out.println("4 - Zakończ program");
+            try {
                 int userChoose = scanner.nextInt();
                 if (userChoose > 0 && userChoose < 5) {
                     switch (userChoose) {
@@ -95,16 +97,16 @@ public class Menu {
                             FolderBooks.saveBooks();
                             System.out.println("Baza książek zapisana poprawnie");
                             isContinue = false;
+                            System.exit(0);
                         }
                     }
                 } else {
-                    System.out.println("Wybierz poprawny numer menu");
-                    continue;
+                    System.out.println("Został wprowadzony niewłaściwy numer.");
                 }
+            } catch (InputMismatchException e) {
+                System.out.println("Został wprowadzony niewłaściwy znak.");
+                scanner.next();
             }
-        } catch (Exception e) {
-            System.out.println("Wprowadziłeś niepoprawny znak");
-            userMenu();
         }
     }
 }
