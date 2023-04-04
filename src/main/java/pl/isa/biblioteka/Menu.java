1eccf96938815531b8794c59f30ac08f51e12e5f
KozlowskiKamil <kamil2k8@gmail.com>
2023-04-04 12:32:03 +0200

    Poprawa wymaganych zmian do PR

--- src/main/java/pl/isa/biblioteka/Menu.java
+++ src/main/java/pl/isa/biblioteka/Menu.java
@@ -15,14 +15,14 @@ public class Menu {
             System.out.println("3 - Zakończ program");
             try {
                 int userChoose = scanner.nextInt();
-                if (userChoose == 1) {
-                    librarianMenu();
-                } else if (userChoose == 2) {
-                    userMenu();
-                } else if (userChoose == 3) {
-                    isContinue = false;
-                } else {
-                    System.out.println("Został wprowadzony niewłaściwy numer\n");
+                switch (userChoose) {
+                    case 1 -> librarianMenu();
+                    case 2 -> userMenu();
+                    case 3 -> {
+                        isContinue = false;
+                        System.exit(0);
+                    }
+                    default -> System.out.println("Został wprowadzony niewłaściwy numer.\n");
                 }
             } catch (InputMismatchException e) {
                 System.out.println("Został wprowadzony niewłaściwy znak\n");
@@ -48,25 +48,22 @@ public class Menu {
             System.out.println("8 - Zakończ program");
             try {
                 int userChoose = scanner.nextInt();
-                if (userChoose > 0 && userChoose < 9) {
-                    switch (userChoose) {
-                        case 1 -> booksEdit.showAllBooks();
-                        case 2 -> users.addUser();
-                        case 3 -> users.deleteUser();
-                        case 4 -> users.listsUsers();
-                        case 5 -> booksEdit.addBook();
-                        case 6 -> booksEdit.deleteBookByTitle();
-                        case 7 -> selectUser();
-                        case 8 -> {
-                            PersonService.saveUsers();
-                            FolderBooks.saveBooks();
-                            System.out.println("Baza użytkowników i książek zapisana poprawnie");
-                            isContinue = false;
-                            System.exit(0);
-                        }
+                switch (userChoose) {
+                    case 1 -> booksEdit.showAllBooks();
+                    case 2 -> users.addUser();
+                    case 3 -> users.deleteUser();
+                    case 4 -> users.listsUsers();
+                    case 5 -> booksEdit.addBook();
+                    case 6 -> booksEdit.deleteBookByTitle();
+                    case 7 -> selectUser();
+                    case 8 -> {
+                        PersonService.saveUsers();
+                        FolderBooks.saveBooks();
+                        System.out.println("Baza użytkowników i książek zapisana poprawnie");
+                        isContinue = false;
+                        System.exit(0);
                     }
-                } else {
-                    System.out.println("Został wprowadzony niewłaściwy numer.");
+                    default -> System.out.println("Został wprowadzony niewłaściwy numer.");
                 }
             } catch (InputMismatchException e) {
                 System.out.println("Został wprowadzony niewłaściwy znak.");
@@ -88,20 +85,17 @@ public class Menu {
             System.out.println("4 - Zakończ program");
             try {
                 int userChoose = scanner.nextInt();
-                if (userChoose > 0 && userChoose < 5) {
-                    switch (userChoose) {
-                        case 1 -> booksEdit.showAllBooks();
-                        case 2 -> borrowBooks.mainLoop();
-                        case 3 -> selectUser();
-                        case 4 -> {
-                            FolderBooks.saveBooks();
-                            System.out.println("Baza książek zapisana poprawnie");
-                            isContinue = false;
-                            System.exit(0);
-                        }
+                switch (userChoose) {
+                    case 1 -> booksEdit.showAllBooks();
+                    case 2 -> borrowBooks.mainLoop();
+                    case 3 -> selectUser();
+                    case 4 -> {
+                        FolderBooks.saveBooks();
+                        System.out.println("Baza książek zapisana poprawnie");
+                        isContinue = false;
+                        System.exit(0);
                     }
-                } else {
-                    System.out.println("Został wprowadzony niewłaściwy numer.");
+                    default -> System.out.println("Został wprowadzony niewłaściwy numer.");
                 }
             } catch (InputMismatchException e) {
                 System.out.println("Został wprowadzony niewłaściwy znak.");
