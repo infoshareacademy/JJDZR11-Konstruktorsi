1cdfdd3219599490dcd864408579658dd4340086
KozlowskiKamil <kamil2k8@gmail.com>
2023-04-17 01:16:00 +0200

    3 subtaski: Dodanie multi-project Konfiguracja pliku application.properties Stworzenie struktury web

--- src/main/java/pl/isa/biblioteka/LibraryApp.java
+++ src/main/java/pl/isa/biblioteka/LibraryApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BibliotekaApplication {
+public class LibraryApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BibliotekaApplication.class, args);
-	}
+		SpringApplication.run(LibraryApp.class, args);
 
+	}
 }
--- src/test/java/pl/isa/biblioteka/LibraryAppTests.java
+++ src/test/java/pl/isa/biblioteka/LibraryAppTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BibliotekaApplicationTests {
+class LibraryAppTests {
 
 	@Test
 	void contextLoads() {
