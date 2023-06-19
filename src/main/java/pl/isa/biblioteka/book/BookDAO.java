61db568b7ac10114e386014dbfdee64f5738f6c1
KozlowskiKamil <kamil2k8@gmail.com>
2023-06-19 21:08:19 +0200

    Zapis person plusk DAO

---
+++ src/main/java/pl/isa/biblioteka/book/BookDAO.java
@@ -0,0 +1,18 @@
+package pl.isa.biblioteka.book;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookDAO {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Transactional
+    public Book saveBook(Book book) {
+        return book;
+    }
+}
--- src/test/java/pl/isa/biblioteka/LibraryAppTests.java
+++ src/test/java/pl/isa/biblioteka/LibraryAppTests.java
@@ -1,10 +1,27 @@
 package pl.isa.biblioteka;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import pl.isa.biblioteka.user.Person;
+import pl.isa.biblioteka.user.PersonDAO;
 
 @SpringBootTest
 class LibraryAppTests {
+
+	@Autowired
+	PersonDAO personDAO;
+
+	@Test
+	void savePerson() {
+		Person person = new Person(
+				"KamilLogin",
+				"KamilPass",
+				"Kamil",
+				"Koz",
+				"k@k.pl");
+		personDAO.savePerson(person);
+	}
 
 	@Test
 	void contextLoads() {
